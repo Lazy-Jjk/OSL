4a89f66ce9da0b35c0ea2fc9c7ed72fd6eab80e1
Lazy-Jjk <161107455+Lazy-Jjk@users.noreply.github.com>
2024-12-12 08:48:14 +0530

    Update sjf non.cpp

--- sjf non.cpp
+++ sjf non.cpp
@@ -1,64 +1,60 @@
-include <stdio.h>
 
-int main() {
-    int n;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-
-    int arrivalTime[n], burstTime[n], waitingTime[n], turnAroundTime[n];
-    int completionTime[n], isCompleted[n];
-    int currentTime = 0, completed = 0;
-    float totalWaitingTime = 0, totalTurnAroundTime = 0;
-
-    // Initialize all processes as incomplete
-    for (int i = 0; i < n; i++) {
-        isCompleted[i] = 0;
-    }
-
-    // Input arrival and burst times
-    for (int i = 0; i < n; i++) {
-        printf("Enter arrival time for process %d: ", i + 1);
-        scanf("%d", &arrivalTime[i]);
-        printf("Enter burst time for process %d: ", i + 1);
-        scanf("%d", &burstTime[i]);
-    }
-
-    // SJF Scheduling calculations
-    while (completed < n) {
-        int minBurstTime = 1e9; // large initial value
-        int shortestJob = -1;
-
-        // Find the process with the minimum burst time that has arrived and is not completed
-        for (int i = 0; i < n; i++) {
-            if (arrivalTime[i] <= currentTime && !isCompleted[i] && burstTime[i] < minBurstTime) {
-                minBurstTime = burstTime[i];
-                shortestJob = i;
-            }
-        }
-
-        if (shortestJob == -1) {
-            currentTime++; // if no process has arrived, increment time
-        } else {
-            // Process found with shortest burst time
-            currentTime += burstTime[shortestJob];
-            completionTime[shortestJob] = currentTime;
-            turnAroundTime[shortestJob] = completionTime[shortestJob] - arrivalTime[shortestJob];
-            waitingTime[shortestJob] = turnAroundTime[shortestJob] - burstTime[shortestJob];
-
-            totalWaitingTime += waitingTime[shortestJob];
-            totalTurnAroundTime += turnAroundTime[shortestJob];
-            isCompleted[shortestJob] = 1;
-            completed++;
-        }
-    }
-y
-    printf("\nProcess\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, arrivalTime[i], burstTime[i], waitingTime[i], turnAroundTime[i]);
-    }
-
-    printf("\nAverage Waiting Time: %.2f", totalWaitingTime / n);
-    printf("\nAverage Turnaround Time: %.2f\n", totalTurnAroundTime / n);
-
-    return 0;
+#include <stdio.h>
+#include <limits.h>
+void findWaitingTime(int n, int bt[], int at[], int wt[]) {
+ int rt[n];
+ int complete = 0, t = 0;
+ int minm = INT_MAX, shortest = 0;
+ int check = 0;
+ for (int i = 0; i < n; i++) {
+rt[i] = bt[i];
+ wt[i] = 0;
+ }
+ while (complete != n) {
+ for (int j = 0; j < n; j++) {
+ if (at[j] <= t && rt[j] < minm && rt[j] > 0) {
+ minm = rt[j];
+ shortest = j;
+ check = 1;
+ }
+ }
+ if (check == 0) {
+ t++;
+ continue;
+ }
+ rt[shortest]--;
+ minm = rt[shortest];
+ if (minm == 0) minm = INT_MAX;
+ if (rt[shortest] == 0) {
+ complete++;
+ check = 0;
+ wt[shortest] = t + 1 - bt[shortest] - at[shortest];
+ if (wt[shortest] < 0) wt[shortest] = 0;
+ }
+ t++;
+ }
 }
+void findTurnAroundTime(int n, int bt[], int wt[], int tat[]) {
+ for (int i = 0; i < n; i++) {
+ tat[i] = bt[i] + wt[i];
+ }
+}
+void findAvgTime(int n, int bt[], int at[]) {
+ int wt[n], tat[n];
+ int totalWT = 0, totalTAT = 0;
+ findWaitingTime(n, bt, at, wt);
+ findTurnAroundTime(n, bt, wt, tat);
+ printf("P\tAT\tBT\tWT\tTAT\n");
+ for (int i = 0; i < n; i++) {
+ totalWT += wt[i];
+ totalTAT += tat[i];
+ printf("%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], wt[i], tat[i]);
+ }
+ printf("Average Waiting Time = %.2f\n", (float)totalWT / n);
+ printf("Average Turnaround Time = %.2f\n", (float)totalTAT / n);
+}
+int main() {
+ int at[] = {1, 3, 3, 3}; 
+ int bt[] = {2, 2, 4, 4}; 
+ int n = sizeof(bt) / sizeof(bt[0]);
+ findAvgTime(n, bt, at);
